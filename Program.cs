7dc7de8c34e2f2f9a2258db157a3114d93e8a026
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-08-09 11:26:01 -0300

    AddScoped DI

--- Program.cs
+++ Program.cs
@@ -2,9 +2,11 @@ using DependencyStore.Repositories;
 using DependencyStore.Repositories.Contracts;
 using DependencyStore.Services;
 using DependencyStore.Services.Contracts;
+using Microsoft.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddScoped(c => new SqlConnection("CONNECTION_STRING"));
 builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
 builder.Services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
 builder.Services.AddTransient<IDeliveryFeeService, DeliveryFeeService>();
